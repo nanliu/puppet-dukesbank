9801f1769e2be151bede9a15159d0572507a53e0
Nan Liu <nan@puppetlabs.com>
2012-02-09 12:31:41 -0800

    Add Duke's bank database.

---
+++ manifests/db.pp
@@ -0,0 +1,47 @@
+# class dukesbank::db
+#
+#
+class dukesbank::db (
+  $database = 'dukes_db',
+  $username = 'dukes_admin',
+  $password = 'dukes_pwd'
+) {
+
+  database { $database:
+    ensure   => present,
+    charset  => 'utf8',
+    provider => 'mysql',
+  }
+
+  database_user { "${username}@localhost":
+    ensure        => present,
+    password_hash => mysql_password($password),
+    require       => Database[$database],
+  }
+
+  database_user { $username:
+    ensure        => present,
+    password_hash => mysql_password($password),
+    require       => Database[$database],
+  }
+
+  database_grant { "${username}@localhost/${database}":
+    privileges => [ 'all' ],
+    require    => Database_user["${username}@localhost"],
+  }
+  database_grant { "${username}/${database}":
+    privileges => [ 'all' ],
+    require    => Database_user[$username],
+  }
+
+  $sql = template('dukesbank/create_dukes_bank.sql.erb')
+
+  exec { "${database}-import":
+    command     => "/usr/bin/mysql -u ${username} -p${password} -h localhost ${database} < ${sql}",
+    logoutput   => true,
+    refreshonly => true,
+    subscribe   => Database[$database],
+    require     => Database_grant["${username}@localhost/${database}", "${username}/${database}"],
+  }
+
+}
